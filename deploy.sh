1017480d194cae1a22a5bc4d1662bbe6f5735ba7
grusza <info@grusza.tv>
2019-05-24 08:35:44 +0100

    addeddeployment scripts

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+docker build -t grusza/sslv-client:latest -t grusza/sslv-client:$SHA -f ./client/Dockerfile ./client
+docker build -t grusza/sslv-server:latest -t grusza/sslv:server:$SHA -f ./server/Dockerfile ./server
+docker build -t grusza/sslv-worker:latest -t grusza/sslv-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push grusza/sslv-client:latest
+docker push grusza/sslv-server:latest
+docker push grusza/sslv-worker:latest
+
+docker push grusza/sslv-client:$SHA
+docker push grusza/sslv-server:$SHA
+docker push grusza/sslv-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=grusza/sslv-server:$SHA
+kubectl set image deployments/client-deployment client=grusza/sslv-client:$SHA
+kubectl set image deployments/worker-deployment client=grusza/sslv-worker:$SHA
