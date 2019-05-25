2b38074179d08b0013f9d9f8b3a1303e1539bee6
grusza <info@grusza.tv>
2019-05-25 17:54:03 +0100

    AAA

--- server/index.js
+++ server/index.js
@@ -1,68 +1,26 @@
 const keys = require('./keys');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const Firestore = require('@google-cloud/firestore');
 
-// Express App Setup
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+
+const db = new Firestore({
+  projectId: 'qualified-gist-239722',
+  keyFilename: 'service-accounts.json'
+});
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
 
-// Postgres Client Setup
-const { Pool } = require('pg');
-const pgClient = new Pool({
-  user: keys.pgUser,
-  host: keys.pgHost,
-  database: keys.pgDatabase,
-  password: keys.pgPassword,
-  port: keys.pgPort
-});
-pgClient.on('error', () => console.log('Lost PG connection'));
+app.use(cors("*"));
 
-pgClient
-  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch(err => console.log(err));
+var docRef = db.collection('users').doc('alovelace');
 
-// Redis Client Setup
-const redis = require('redis');
-const redisClient = redis.createClient({
-  host: keys.redisHost,
-  port: keys.redisPort,
-  retry_strategy: () => 1000
-});
-const redisPublisher = redisClient.duplicate();
-
-// Express route handlers
-
-app.get('/', (req, res) => {
-  res.send('Hi');
-});
-
-app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
-
-  res.send(values.rows);
-});
-
-app.get('/values/current', async (req, res) => {
-  redisClient.hgetall('values', (err, values) => {
-    res.send(values);
-  });
-});
-
-app.post('/values', async (req, res) => {
-  const index = req.body.index;
-
-  if (parseInt(index) > 40) {
-    return res.status(422).send('Index too high');
-  }
-
-  redisClient.hset('values', index, 'Nothing yet!');
-  redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
-
-  res.send({ working: true });
+var setAda = docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 77777
 });
 
 app.listen(5000, err => {
--- server/keys.js
+++ server/keys.js
@@ -1,9 +1,12 @@
 module.exports = {
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
-  pgUser: process.env.PGUSER,
-  pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
-  pgPassword: process.env.PGPASSWORD,
-  pgPort: process.env.PGPORT
+  type: process.env.PROJECT_ID,
+  project_id: process.env.MONGO_SECRET,
+  private_key_id: process.env.MONGO_URI,
+  private_key: process.env.MONGO_PORT,
+  client_email: process.env.MONGO_PORT,
+  client_id: process.env.MONGO_PORT,
+  auth_uri: process.env.MONGO_PORT,
+  token_uri: process.env.MONGO_PORT,
+  auth_provider_x509_cert_url: process.env.MONGO_PORT,
+  client_x509_cert_url: process.env.MONGO_PORT,
 };
