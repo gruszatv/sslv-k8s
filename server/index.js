c14020ce0f5c69d4b03db242edf4ff8cef7d4e28
grusza <info@grusza.tv>
2019-05-26 15:40:28 +0100

    auto reload fixed, the problem was with docker

--- server/index.js
+++ server/index.js
@@ -9,45 +9,22 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Postgres Client Setup
-const { Pool } = require('pg');
-const pgClient = new Pool({
-  user: keys.pgUser,
-  host: keys.pgHost,
-  database: keys.pgDatabase,
-  password: keys.pgPassword,
-  port: keys.pgPort
-});
-pgClient.on('error', () => console.log('Lost PG connection'));
-
-pgClient
-  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch(err => console.log(err));
-
-// Redis Client Setup
-const redis = require('redis');
-const redisClient = redis.createClient({
-  host: keys.redisHost,
-  port: keys.redisPort,
-  retry_strategy: () => 1000
-});
-const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
 app.get('/', (req, res) => {
-  res.send('Hi');
+  res.send('Hi Alex');
 });
 
 app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
 
-  res.send(values.rows);
+  res.send("AAAAA");
 });
 
 app.get('/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
-    res.send(values);
+    res.send('BBBBBB');
   });
 });
 
@@ -58,11 +35,8 @@ app.post('/values', async (req, res) => {
     return res.status(422).send('Index too high');
   }
 
-  redisClient.hset('values', index, 'Nothing yet!');
-  redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-  res.send({ working: true });
+  res.send('CCCC');
 });
 
 app.listen(5000, err => {
