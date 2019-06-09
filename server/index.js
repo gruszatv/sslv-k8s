bed164d10c4c4d90be9eeca37f5cbcc714cc04c6
Aleksejs Sitdikovs <aleksejssitdikovs@aleksejss-mbp.home>
2019-06-09 15:08:55 +0100

    plenty of things are done: skaffold, local development, nginx ingress

--- server/index.js
+++ server/index.js
@@ -22,8 +22,9 @@ const schema = makeExecutableSchema({
 });
 
 // Connects to database
+// mongodb://reed:reed@ds137720.mlab.com:37720/react-recipes
 mongoose
-    .connect("mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo:27017/dbname_?")
+    .connect("mongodb://reed:reed@ds137720.mlab.com:37720/react-recipes")
     .then(() => console.log("DB connected"))
     .catch(err => console.error(err));
 
